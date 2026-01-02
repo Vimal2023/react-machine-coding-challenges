72fe4b0efa6861c631887d5615324e0ed5dfa43f
Vimal2023 <vimal83500@gmail.com>
2026-01-02 17:16:57 +0530

    faq component completed

---
+++ faq-component/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import FAQComp from "./FAQComp";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FAQ Component</h1>
+      <FAQComp />
+    </div>
+  );
+}
+
+export default App;
