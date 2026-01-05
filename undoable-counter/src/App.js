d8855bb3c6214265e39a120b490cbf63552b4a04
Vimal2023 <vimal83500@gmail.com>
2026-01-05 13:22:04 +0530

    completed the undoable-counter

--- undoable-counter/src/App.js
+++ undoable-counter/src/App.js
@@ -1,10 +1,77 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [history, setHistory] = useState([]);
+  const [redoList, setRedoList] = useState([]);
+  const [undoCount, setUndoCount] = useState(0);
+  const maintainHistroy = (key, prev, curr) => {
+    const obj = {
+      action: key,
+      prev,
+      curr,
+    };
+    const copyHistory = [...History];
+    copyHistory.unshift(obj);
+    setHistory(copyHistory);
+  };
+  const handleClick = (key) => {
+    const val = parseInt(key);
+    maintainHistroy(key, value, val + value);
+    setValue((existingValue) => existingValue + val);
+  };
+  const handleUndo = () => {
+    if (history.length) {
+      if (undoCount + 1 > 5) {
+        alert("You cant undo beyond limit=5");
+        return;
+      }
+      const copyHist = [...history];
+      const firstItem = copyHist.shift();
+      setHistory(copyHist);
+      const copyRedoList = [...redoList];
+      copyRedoList.push(firstItem);
+      setRedoList(copyRedoList);
+    }
+  };
+
+  const handleRedo = () => {
+    if (redoList.length) {
+      const copyRedoList = [...redoList];
+      const poppedValue = copyRedoList.pop();
+      setRedoList(copyRedoList);
+      const { action, prev, curr } = poppedValue;
+      setValue(curr);
+      maintainHistroy(action, prev, curr);
+    }
+  };
   return (
     <div className="App">
-      <div className=''></div>
+      <h1>Undoable Counter</h1>
+      <div className="action-btn">
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleRedo}>Redo</button>
+      </div>
+      <div className="user-actions">
+        {[-100, -10, -1].map((btn) => {
+          return <button onClick={() => handleClick(btn)}>{btn}</button>;
+        })}
+        <div style={{ fontSize: 40 }}>{value}</div>
+        {["+1", "+10", "+100"].map((btn) => {
+          return <button onClick={() => handleClick(btn)}>{btn}</button>;
+        })}
+      </div>
+      <div className="history">
+        {history.map((item) => {
+          return (
+            <div className="row">
+              <div>{item.action}</div>
+              <div>{`${item.prev} -> ${item.curr}`}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
