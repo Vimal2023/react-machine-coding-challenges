b0b3578e96e4a24f4f8abf7a2ed40b3f2e184f0d
Vimal2023 <vimal83500@gmail.com>
2026-01-06 23:21:08 +0530

    completed the shopping list

---
+++ shopping-list/src/App.js
@@ -0,0 +1,87 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [food, setFood] = useState("");
+  const [shoppingList, setShoppingList] = useState([]);
+  const [bucketList, setBucketList] = useState([]);
+  const handleInput = (e) => {
+    console.log(e.target.value);
+    setFood(e.target.value);
+  };
+
+  const fetchItems = async (food) => {
+    const url = `https://api.frontendeval.com/fake/food/${food}`;
+    const result = await fetch(url);
+    const data = await result.json();
+    setShoppingList(data);
+  };
+  const handleShippingList = (e) => {
+    const idx = e.target.getAttribute("data-id");
+    if (idx) {
+      const obj = {
+        id: Date.now(),
+        data: shoppingList[idx],
+        isDone: false,
+      };
+      const copyBucketList = [...bucketList];
+      copyBucketList.push(obj);
+      setBucketList(copyBucketList);
+    }
+    setFood("");
+  };
+
+  const handleRightClick = (id) => {
+    const copyBucketList = [...bucketList];
+    const newBucketList = copyBucketList.map((item) => {
+      if (item.id === id) {
+        item.isDone = !item.isDone;
+      }
+      return item;
+    });
+    setBucketList(newBucketList);
+  };
+
+  const handleDelete =(id) => {
+    const copyBucketList = [...bucketList];
+    const newList = copyBucketList.filter((item) => item.id !== id);
+    setBucketList(newList);
+  }
+  useEffect(() => {
+    if (food.length >= 2) {
+      fetchItems(food);
+    }
+  }, [food]);
+  return (
+    <div className="App">
+      <h1>My Shopping List</h1>
+      <div>
+        <input value={food} onChange={handleInput} />
+      </div>
+      {food.length >= 2 ? (
+        <div className="shopping-list" onClick={handleShippingList}>
+          {shoppingList.map((item, index) => {
+            return (
+              <div data-id={index} className="product">
+                {item}
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
+      <div className="bucket">
+        {bucketList.map((item) => {
+          return (
+            <div className="shopping-item">
+              <button onClick={() => handleRightClick(item.id)}>✓</button>
+              <div className={item.isDone ? 'strik' : ''}>{item.data}</div>
+              <button onClick={() => handleDelete(item.id)}>X</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
