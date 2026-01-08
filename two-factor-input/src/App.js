1cf38681d93ab0509955e65f798ae32ab845ab7e
Vimal2023 <vimal83500@gmail.com>
2026-01-08 17:09:34 +0530

    completed the two factor input

---
+++ two-factor-input/src/App.js
@@ -0,0 +1,87 @@
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  const emptyArr = ["", "", "", ""];
+  const refs = [useRef(), useRef(), useRef(), useRef()];
+  const [inputs, setInputs] = useState(emptyArr);
+  const [missing, setMissing] = useState(emptyArr);
+  const CODE = '1234';
+
+  useEffect(() => {
+    refs[0].current.focus();
+  }, []);
+
+  const handleInputChange = (e, index) => {
+    const val = e.target.value;
+    if (!Number(val)) return;
+    if (index < inputs.length - 1) {
+      refs[index + 1].current.focus();
+    }
+    const copyInputs = [...inputs];
+    copyInputs[index] = val;
+    setInputs(copyInputs);
+  };
+  const handleOnKeyDown = (e, index) => {
+    if (e.keyCode === 8) {
+      const copyInputs = [...inputs];
+      copyInputs[index] = "";
+      setInputs(copyInputs);
+
+      if (index > 0) {
+        refs[index - 1].current.focus();
+      }
+    }
+  };
+  const handlePaste = (e) => {
+    const data = e.clipboardData.getData("text");
+    if (!Number(data) || data.length !== inputs.length) return;
+
+    const pasteCode = data.split("");
+    setInputs(pasteCode);
+    refs[inputs.length - 1].current.focus();
+  };
+
+  const handleSubmit = () => {
+    const missed = inputs
+      .map((item, i) => {
+        if (item === "") return i;
+      })
+      .filter((item) => item || item === 0);
+      setMissing(missed);
+
+      if(missed.length){
+        return
+      }
+
+      const userInput = inputs.join('');
+      const isMatch = userInput === CODE;
+      const msg = isMatch ? "Code is correct" : "code is not correct";
+      alert(msg);
+  };
+  return (
+    <div className="App">
+      <h1>Two-factor code Input</h1>
+      <div>
+        {emptyArr.map((item, i) => {
+          return (
+            <input
+              value={inputs[i]}
+              key={i}
+              ref={refs[i]}
+              type="text"
+              maxLength="1"
+              onPaste={handlePaste}
+              onChange={(e) => handleInputChange(e, i)}
+              onKeyDown={(e) => handleOnKeyDown(e, i)}
+              className={missing.includes(i) ? 'error' : ''}
+            />
+          );
+        })}
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
